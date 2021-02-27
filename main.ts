66b86abdaa96bd08d9dafa80c1b5732608ee79a0
Caleb Lam <67606076+CalebLam14@users.noreply.github.com>
2021-02-27 15:19:19 -0500

    Completed game

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const Prop = SpriteKind.create()
+}
+
 let inputImgs: Image[] = [
     assets.image`A`,
     assets.image`B`,
@@ -17,48 +21,102 @@ let inputTypes: (controller.Button)[] = [
 ]
 
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+	addAndCheckPlayerInput(0)
 })
 
-function addPlayerInput (inputType: number) {
-	
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+	addAndCheckPlayerInput(1)
+})
+
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+	addAndCheckPlayerInput(2)
+})
+
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+	addAndCheckPlayerInput(3)
+})
+
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+	addAndCheckPlayerInput(4)
+})
+
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+	addAndCheckPlayerInput(5)
+})
+
+function displayText(text: string, color: number = 0) {
+    gameText.setText(text)
+    gameText.setOutline(1, color)
+    gameText.setPosition(80, 30)
+}
+
+function addAndCheckPlayerInput (inputType: number) {
+	if (playerTurn) {
+        let correctInput = sequence[currentSequenceIndex]
+
+        inputSprite.setImage(inputImgs[inputType])
+        if (inputType == correctInput) {
+            music.baDing.play()
+            currentSequenceIndex++
+
+            if (currentSequenceIndex == sequenceSize) {
+                playerTurn = false
+                info.changeScoreBy(1)
+                displayText("Nice job!", 7)
+                music.magicWand.playUntilDone()
+                pause(1000)
+                sequenceSize++
+                beginNextRound()
+            }
+        }
+        else {
+            displayText("Wrong!", 2)
+            music.buzzer.playUntilDone()
+            pause(1000)
+            displayText("The correct one is...", 2)
+            inputSprite.setImage(inputImgs[correctInput])
+            pause(2000)
+            game.over(false)
+        }
+    }
 }
 
 function beginNextRound () {
+    playerTurn = false
+    currentSequenceIndex = 0
+    sequence = []
+    inputSprite.setImage(assets.image`QuestionMark`)
+    displayText("Watch & remember!")
+    gameText.setPosition(80, 30)
+    pause(1000)
+
     for (let index = 0; index < sequenceSize; index++) {
         let chosenInputIndex: number = randint(0, inputTypes.length - 1)
         let chosenInput: controller.Button = inputTypes[chosenInputIndex]
         let chosenInputImg: Image = inputImgs[chosenInputIndex]
-        sequence.push(chosenInput)
+        sequence.push(chosenInputIndex)
         inputSprite.setImage(chosenInputImg)
+        music.zapped.play()
+        pause(750)
     }
+
+    pause(500)
+    inputSprite.setImage(assets.image`QuestionMark`)
+    playerTurn = true
 }
+
+let playerTurn: boolean = false
 
 let sequenceSize: number = 1
 
 let currentSequenceIndex = 0
-let sequence: (controller.Button)[] = []
-let playerSequence: number[] = []
+let sequence: number[] = []
 
-let textSprite: TextSprite = textsprite.create("Watch & Remember!")
-textSprite.setPosition(80, 30)
-let inputSprite: Sprite = sprites.create(img`
-    . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
-    . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
-    . . . . . . . . . . . . 1 1 . . 
-    . . . . . . . . . . . . 1 1 . . 
-    . . . . . . . . . . . . 1 1 . . 
-    . . . . . . . . . . . . 1 1 . . 
-    . . . . . . . 1 1 1 1 1 1 1 . . 
-    . . . . . . . 1 1 1 1 1 1 1 . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    `, 0)
+let gameText: TextSprite = textsprite.create("???")
+gameText.setPosition(80, 30)
+
+let inputSprite: Sprite = sprites.create(assets.image`QuestionMark`, SpriteKind.Prop)
+
+info.setScore(0)
 beginNextRound()
  
